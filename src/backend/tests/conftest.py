e9bcc86df6066c02cdd4845380844176e6487b01
Daniel Nozka <danozka@gmail.com>
2024-01-06 11:10:09 +0100

    Modifies conftest.py

--- src/backend/tests/conftest.py
+++ src/backend/tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 import threading
 
 from dependency_injector.wiring import inject, Provide
+from threading import Timer
 
 from . import fixtures
 from tubefy import APP_COMPONENTS, ModuleInitializer
@@ -25,6 +26,7 @@ def setup() -> None:
     setup_logging()
     yield
     close_timer_threads()
+    close_persistence()
     delete_test_data()
 
 
@@ -32,11 +34,13 @@ def setup() -> None:
 def setup_logging(logging_handler: LoggingHandler = Provide['logging_handler']) -> None:
 
     logging_handler.build()
+    yield
+    logging_handler.close()
 
 
 def close_timer_threads():
 
-    running_timer_threads = [thread for thread in threading.enumerate() if isinstance(thread, threading.Timer)]
+    running_timer_threads: list[Timer] = [thread for thread in threading.enumerate() if isinstance(thread, Timer)]
 
     for thread in running_timer_threads:
 
@@ -44,17 +48,21 @@ def close_timer_threads():
 
 
 @inject
-def delete_test_data(
-    app_settings: AppSettings = Provide['app_settings'],
+def close_persistence(
     audio_recordings_persistence: AudioRecordingsPersistence = Provide['audio_recordings_persistence'],
     audio_samples_persistence: AudioSamplesPersistence = Provide['audio_samples_persistence'],
-    users_persistence: UsersPersistence = Provide['users_persistence'],
-    directory_handler: DirectoryHandler = Provide['directory_handler'],
-    logging_handler: LoggingHandler = Provide['logging_handler']
+    users_persistence: UsersPersistence = Provide['users_persistence']
 ) -> None:
 
     audio_recordings_persistence.close()
     audio_samples_persistence.close()
     users_persistence.close()
-    logging_handler.close()
+
+
+@inject
+def delete_test_data(
+    app_settings: AppSettings = Provide['app_settings'],
+    directory_handler: DirectoryHandler = Provide['directory_handler']
+) -> None:
+
     directory_handler.delete_directory(app_settings.persistence_settings.data_path)
