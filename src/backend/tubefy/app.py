91da0e2110f850d9d20b454f2b7347b543943bc6
Daniel Nozka <danozka@gmail.com>
2024-01-06 11:01:36 +0100

    Adds frontend static files serving

--- src/backend/tubefy/app.py
+++ src/backend/tubefy/app.py
@@ -4,6 +4,7 @@ import uvicorn
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
 from logging import Logger
 from pathlib import Path
 from types import ModuleType
@@ -31,9 +32,9 @@ class App(FastAPI):
     def __init__(self, host: str, port: int):
 
         super().__init__(docs_url='/api/docs', openapi_url='/api/openapi.json')
-        self.add_exception_handler(AppBaseException, self._handle_app_exception)
-        self.add_exception_handler(Exception, self._handle_unexpected_exception)
         self._add_controllers()
+        self._add_exception_handlers()
+        self._add_frontend()
         self._add_middleware()
         self._host = host
         self._port = port
@@ -44,7 +45,7 @@ class App(FastAPI):
 
         try:
 
-            uvicorn.run(self, host=self._host, port=self._port)
+            uvicorn.run(app=self, host=self._host, port=self._port)
 
         except Exception as exception:
 
@@ -57,9 +58,27 @@ class App(FastAPI):
             app_controller = AppController()
             self.include_router(app_controller.api_router)
 
+    def _add_exception_handlers(self) -> None:
+
+        self.add_exception_handler(exc_class_or_status_code=AppBaseException, handler=self._handle_app_exception)
+        self.add_exception_handler(exc_class_or_status_code=Exception, handler=self._handle_unexpected_exception)
+
+    def _add_frontend(self) -> None:
+
+        frontend_build_directory = APP_ROOT_PATH.joinpath('static').joinpath('browser')
+        frontend_index_html_path = frontend_build_directory.joinpath('index.html')
+
+        if frontend_index_html_path.is_file():
+
+            self.mount(path='/', app=StaticFiles(directory=frontend_build_directory, html=True), name='frontend')
+
+        else:
+
+            self._log.warning('Frontend build content not found')
+
     def _add_middleware(self) -> None:
 
-        self.add_middleware(CorrelationIdMiddleware, generator=lambda: str(uuid4()))
+        self.add_middleware(middleware_class=CorrelationIdMiddleware, generator=lambda: str(uuid4()))
 
     @staticmethod
     async def _handle_app_exception(request: Request, exc: AppBaseException) -> JSONResponse:
